8e3ead5dfab1ae4f8800338193f1339a4218f6bd
gerfesson <gerfessonsn@gmail.com>
2019-12-14 22:40:59 -0300

    🚧 corrigindo proptypes

--- modulo2/app1/src/components/CompareList/index.js
+++ modulo2/app1/src/components/CompareList/index.js
@@ -6,7 +6,7 @@ import { Container, Repository } from './styles';
 const CompareList = ({ repositories }) => (
   <Container>
     {repositories.map((repository) => (
-      <Repository key={repository.name}>
+      <Repository key={repository.id}>
         <header>
           <img src={repository.owner.avatar_url} alt={repository.owner.login} />
           <strong>{repository.name}</strong>
@@ -33,11 +33,24 @@ const CompareList = ({ repositories }) => (
 );
 
 CompareList.defaultProps = {
-  repositories: [],
+  // repositories: [],
 };
 
 CompareList.propTypes = {
-  repositories: PropTypes.array,
+  repositories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      owner: PropTypes.shape({
+        login: PropTypes.string,
+        avatar_url: PropTypes.string,
+      }),
+      stargazers_count: PropTypes.number,
+      forks_count: PropTypes.number,
+      open_issues_count: PropTypes.number,
+      pushed_at: PropTypes.string,
+    }),
+  ).isRequired,
 };
 
 export default CompareList;
