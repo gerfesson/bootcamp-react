29d3a411c7ffbb93f582915eadad2f442839c90b
gerfesson <gerfessonsn@gmail.com>
2020-01-08 23:05:08 -0300

    🚧 controle no reducer e alteração no componente

--- modulo3/src/TodoList.js
+++ modulo3/src/TodoList.js
@@ -2,13 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as TodoActions from './store/actions/todos';
 
 const TodoList = (props) => {
-  const { todos } = props;
+  const { todos, addTodo, removeTodo } = props;
   return (
-    <ul>
-      {todos.map((todo) => <li key={todo.id}>{todo.text}</li>)}
-    </ul>
+    <>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button type="button" onClick={() => removeTodo(todo.id)}>Remover</button>
+          </li>
+        ))}
+      </ul>
+      <button type="button" onClick={() => addTodo('Novo todo')}>Adicionar</button>
+    </>
   );
 };
 
@@ -17,10 +27,14 @@ TodoList.propTypes = {
     id: PropTypes.number,
     text: PropTypes.string,
   })).isRequired,
+  addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   todos: state.todos,
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = (dispatch) => bindActionCreators(TodoActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
