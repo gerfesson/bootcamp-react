d431f802c67fefb60027a33c9461e9dc74bb707a
gerfesson <gerfessonsn@gmail.com>
2020-01-11 20:16:28 -0300

    🚧 adicionando footer

--- modulo3/src/routes/index.js
+++ modulo3/src/routes/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from '../pages/main/index';
+import Footer from '../components/Footer/index';
 
 const Routes = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Main} />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+      <Footer />
+    </>
   </BrowserRouter>
 );
 
