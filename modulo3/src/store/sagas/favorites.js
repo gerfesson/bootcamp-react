ec8add3dc38cdfe6cd9006630bf68b64cf6503e8
gerfesson <gerfessonsn@gmail.com>
2020-01-27 19:17:53 -0300

    🚧 adicionando duck pattern e controlando duplicidade de itens

--- modulo3/src/store/sagas/favorites.js
+++ modulo3/src/store/sagas/favorites.js
@@ -1,21 +1,27 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 
-import { addFavoriteSuccess, addFavoriteFailure } from '../actions/favorites';
+import { Creators as FavoriteActions } from '../ducks/favorites';
 
 export function* addFavorite(action) {
   try {
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
 
-    const repositoryData = {
-      id: data.id,
-      name: data.full_name,
-      description: data.description,
-      url: data.html_url,
-    };
+    const isDuplicated = yield select((state) => state.favorites.data.find((favorite) => favorite.id === data.id));
 
-    yield put(addFavoriteSuccess(repositoryData));
+    if (isDuplicated) {
+      yield put(FavoriteActions.addFavoriteFailure('Repositório duplicado'));
+    } else {
+      const repositoryData = {
+        id: data.id,
+        name: data.full_name,
+        description: data.description,
+        url: data.html_url,
+      };
+
+      yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+    }
   } catch (err) {
-    yield put(addFavoriteFailure('Erro ao adicionar repositório'));
+    yield put(FavoriteActions.addFavoriteFailure('Erro ao adicionar repositório'));
   }
 }
